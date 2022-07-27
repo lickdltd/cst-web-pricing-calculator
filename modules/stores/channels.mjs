e43305b1d0f52b0dbf8163e98fd199152b2c00ab
Andrew Bridge <andrewbridge@lickd.co>
2022-07-27 23:26:26 +0100

    feat: add a channel lookup store

--- modules/stores/channels.mjs
+++ modules/stores/channels.mjs
@@ -20,4 +20,24 @@ export const channelList = useApiData([], async (channel) => {
         throw Error(message);
     }
     return response.json();
-})
+});
+
+export const channelSearch = useApiData([], async (query) => {
+    const response = await fetch(`https://api.lickd.co/youtube/channels?q=${query}`, {
+        "headers": {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+    if (!response.ok) {
+        let message = response.status + 'An unknown error occurred';
+        try {
+            const errorResponse = await response.json();
+            message = response.status + ' ' + errorResponse.error;
+        } catch {
+            // No body
+        }
+        throw Error(message);
+    }
+    return (await response.json()).data.slice(0, 4);
+});
