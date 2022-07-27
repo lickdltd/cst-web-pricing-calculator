1350d6fdbb386a2d5f028b37124e8e3b66c05c97
Andrew Bridge <andrewbridge@lickd.co>
2022-07-27 23:28:36 +0100

    feat: add complex input interactions to allow for mass copy and paste of Youtube channel IDs and typed search look up

--- modules/components/SearchBar.mjs
+++ modules/components/SearchBar.mjs
@@ -23,15 +23,52 @@ const styles = {
 
 export default {
     components: {},
-    data: () => ({ input: '' }),
-    template: `<form class="${styles.form}" @submit="queryChannel">
-        <input v-model="input" />
-    </form>`,
-    computed: mapState('channels', 'channelList'),
+    data: () => ({ input: '', queries: new Set(), }),
+    template: `<div>
+        <form class="${styles.form}" @submit="searchChannel" @click="$refs.input.focus()">
+            <span v-for="query in queries" class="${styles.pill}">{{query}}</span>
+            <input v-model="input" @keydown.delete="handleBackspace" ref="input" class="${styles.input}" />
+        </form>
+        <div>
+            <div v-for="result in channelSearch.data" @click="addToQueries(result)">
+                {{result.name}}
+            </div>
+        </div>
+        <button>Fetch channel data</button>
+    </div>`,
+    computed: mapState('channels', ['channelList', 'channelSearch']),
+    watch: {
+        input(newInput, oldInput) {
+            if (newInput === '' && this.channelSearch.isFetched) {
+                this.channelSearch.reset();
+                return;
+            }
+            if (Math.abs(newInput.length - oldInput.length) <= 1) return;
+            const validCandidates = newInput.replace(/[^a-zA-Z0-9_-]/g, ' ').split(' ');
+            validCandidates.forEach(candidate => {
+                if (candidate.match(/[a-zA-Z0-9_-]{11}/) === null) return;
+                this.queries.add(candidate);
+            });
+            this.input = '';
+        }
+    },
     methods: {
+        async searchChannel(event) {
+            event.preventDefault();
+            if (this.input.length === 0) return
+            this.channelSearch.fetch(this.input);
+        },
+        addToQueries(channel) {
+            this.queries.add(channel.id);
+            this.$refs.input.focus();
+        },
+        handleBackspace() {
+            if (this.input.length > 0 || this.queries.size === 0) return;
+            const lastQuery = Array.from(this.queries).at(-1);
+            this.queries.delete(lastQuery);
+        },
         queryChannel(event) {
             event.preventDefault();
-            console.log(this.channelList);
             this.channelList.fetch(this.input);
         }
     }
