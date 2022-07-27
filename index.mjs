760e3f4e72a816f7a4f444e38fed104ed804741c
Andrew Bridge <andrewbridge@lickd.co>
2022-07-27 18:50:09 +0100

    feat: Set up a root App component using CSS-in-JS styles

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import { Vue } from './modules/deps.mjs';
+import App from './modules/components/App.mjs';
 
 document.body.innerHTML = '';
 const root = document.createElement('div');
 document.body.appendChild(root);
-Vue.createApp({ template: '<h1>Hello, World!</h1>' }).mount(root);
+Vue.createApp(App).mount(root)''
---
+++ modules/components/App.mjs
@@ -0,0 +1,15 @@
+import { css } from '../deps.mjs';
+import globalStyles from '../globalStyles.mjs';
+
+const styles = {
+    wrapper: css`
+        
+    `
+}
+
+export default {
+    template: `<div class="${globalStyles} ${styles.wrapper}">
+        <h1>Hello, World!</h1>
+    </div>`,
+};
+
